6fcbd54de3974629467dcf5d5dfab5a7e16738a7
Kanav14 <kanavsharma30@gmail.com>
2024-09-25 14:08:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,9 @@ const skillsChart = new Chart(ctx, {
         }
     }
 });
+
+// Dark Mode Toggle
+const toggle = document.getElementById('dark-mode-toggle');
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
